9991f66535ca3b50709f923a6287d39d5ac954fc
Alex Kern <alex@kern.io>
2020-12-17 10:27:03 -0800

    Fix tests

--- src/sms.test.ts
+++ src/sms.test.ts
@@ -24,14 +24,12 @@ test('parses SMS', () => {
 
 test('parses multi-line SMS', () => {
   const inputs = sms.parseSMS({
-    Body: 'fruit smoothie \n1 can of baked beans\n\n',
+    Body: 'fruit smoothie and\n1 can of baked beans\n\n',
   })
 
-  expect(inputs.length).toEqual(2)
-  expect(inputs[0].text).toEqual('fruit smoothie')
+  expect(inputs.length).toEqual(1)
+  expect(inputs[0].text).toEqual('fruit smoothie and. 1 can of baked beans')
   expect(inputs[0].mediaURL).toBeUndefined()
-  expect(inputs[1].text).toEqual('1 can of baked beans')
-  expect(inputs[1].mediaURL).toBeUndefined()
 })
 
 test('parses MMS', () => {
@@ -47,13 +45,11 @@ test('parses MMS', () => {
 
 test('parses multi-line MMS', () => {
   const inputs = sms.parseSMS({
-    Body: 'fruit smoothie \n1 can of baked beans\n\n',
+    Body: 'fruit smoothie and\n1 can of baked beans\n\n',
     MediaUrl0: mediaURL,
   })
 
-  expect(inputs.length).toEqual(2)
-  expect(inputs[0].text).toEqual('fruit smoothie')
+  expect(inputs.length).toEqual(1)
+  expect(inputs[0].text).toEqual('fruit smoothie and. 1 can of baked beans')
   expect(inputs[0].mediaURL).toEqual(mediaURL)
-  expect(inputs[1].text).toEqual('1 can of baked beans')
-  expect(inputs[1].mediaURL).toEqual(mediaURL)
 })
